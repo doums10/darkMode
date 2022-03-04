0a097de9218112f2e1a0036b845ca64836c76663
Mamadou Sylla <>
2022-03-04 13:13:38 +0100

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import "./style.css"
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Bonjour</h1>
-    </div>
-  );
+export default function App() {
+	const [darkMode, setDarkMode] = React.useState(true);
+	function toggle() {
+		setDarkMode((prevDark) => !prevDark);
+	}
+	return (
+		<div className="container">
+			<Navbar darkMode={darkMode} toggleDarkMode={toggle} />
+			<Main darkMode={darkMode} />
+		</div>
+	);
 }
-
-export default App;
